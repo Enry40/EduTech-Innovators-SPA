e0febe53fe6350ecd7b382422277b9e884b856fd
Enrique <e.villarroel@duocuc.cl>
2025-05-18 18:09:31 -0400

    Inicio de proyecto

---
+++ src/main/java/com/EduTechInnovators/cl/EduTech/Innovators/SPA/EduTechInnovatorsSpaApplication.java
@@ -0,0 +1,13 @@
+package com.EduTechInnovators.cl.EduTech.Innovators.SPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduTechInnovatorsSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduTechInnovatorsSpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EduTechInnovators/cl/EduTech/Innovators/SPA/EduTechInnovatorsSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EduTechInnovators.cl.EduTech.Innovators.SPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduTechInnovatorsSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

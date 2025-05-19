92f00b1ef950dcc748a7ee756cbb80d0b1768651
Enrique <e.villarroel@duocuc.cl>
2025-05-19 01:57:09 -0400

    Se finalizan las capas Controller y Service de la clase Administrador.

--- src/main/java/com/EduTechInnovators/cl/EduTech/Innovators/SPA/model/administrador.java
+++ src/main/java/com/EduTechInnovators/cl/EduTech/Innovators/SPA/model/administrador.java
@@ -1,5 +1,6 @@
 package com.EduTechInnovators.cl.EduTech.Innovators.SPA.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -7,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,5 +16,9 @@ import lombok.NoArgsConstructor;
 public class administrador {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private int id_user;
+    private String name;
+    private String password;
+    private String email;
+    private String userType;
 }
--- src/main/java/com/EduTechInnovators/cl/EduTech/Innovators/SPA/repository/administradorRepository.java
+++ src/main/java/com/EduTechInnovators/cl/EduTech/Innovators/SPA/repository/administradorRepository.java
@@ -2,6 +2,8 @@ package com.EduTechInnovators.cl.EduTech.Innovators.SPA.repository;
 
 import com.EduTechInnovators.cl.EduTech.Innovators.SPA.model.administrador;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface administradorRepository  extends JpaRepository<administrador, Integer> {
 }
